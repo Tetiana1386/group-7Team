8e8a8512b3737de1fd88a7132374cc9bccabca3c
Tetiana1386 <tanyashostak13@gmail.com>
2021-02-20 08:45:06 +0200

    Додає розмітку та стилі hero-header

---
+++ src/js/menu.js
@@ -0,0 +1,12 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-button-menu]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+    
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || "false";
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.getAttribute("aria-expanded", !expanded);
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
